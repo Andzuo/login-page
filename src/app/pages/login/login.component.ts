7a3df71754ee1d8e51a5047fde74c5a919a41fd4
Andzuo <alucasm02@gmail.com>
2024-03-25 21:51:43 -0300

    feat: Create initial constructor

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { DefaultPageComponent } from '../../components/default-page/default-page.component';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    DefaultPageComponent
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
